0717a65221285cbe171102f3ce074374136a2ca1
Alexander Loechel <AlexanderLoechel@yahoo.de>
2014-03-26 18:06:25 +0100

    new theme for documentation based on plone.org theme

--- conf.py
+++ conf.py
@@ -100,12 +100,16 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 #html_theme = 'default'
-html_theme = 'plone'
+html_theme = 'ploneorg'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+#	"rightsidebar": "false",
+
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
