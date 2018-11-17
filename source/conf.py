788162c830f1cd446d1aaf7815722d0afe4d1dc6
sven <sven@so36.net>
2018-11-17 10:35:09 +0100

    [docs] Add robots.txt, we do not want crawlers here, yet

--- source/conf.py
+++ source/conf.py
@@ -102,11 +102,13 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['_extra']
 html_favicon = '_static/favicon.ico'
 #html_logo = '_static/logo.svg'
 html_show_copyright = False
 html_show_sphinx = False
 html_show_sourcelink = False
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
