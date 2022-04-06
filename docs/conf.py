c98e89e9f7eb5c9eb48074c7d116d73c9984261d
ksuess <k.suess@rohberg.ch>
2022-04-06 12:02:01 +0200

    Update conf.py ogp_site_url

--- docs/conf.py
+++ docs/conf.py
@@ -174,7 +174,7 @@ graphviz_output_format = "svg"
 
 # -- OpenGraph configuration ----------------------------------
 
-ogp_site_url = "https://training.plone.org/5/"
+ogp_site_url = "https://docs.plone.org/"
 ogp_description_length = 200
 ogp_image = "https://docs.plone.org/_static/Plone_logo_square.png"
 ogp_site_name = "Plone Documentation"
