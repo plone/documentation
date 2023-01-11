079489bc120c7a6d61ef663460dfc5d5a75b4773
MAX-786 <hussainmohammad328@gmail.com>
2023-01-11 22:19:58 +0530

    implement suggestions from #1421

--- docs/_static/search_shortcut.js
+++ docs/_static/search_shortcut.js
@@ -2,37 +2,27 @@
  * Add shortcut `ctrl+k` to focus on search field
  */
 
-$(document).ready(() => {
-    $(document).keydown(function(event) {
-        if ((event.ctrlKey || event.metaKey) && event.key == "k") {
-            event.preventDefault();
-            $('#search-input').focus();
-        }
-    });
-    if (navigator.platform.indexOf('Mac') === -1) {
-        $('#search-shortcut-sidebar').html("^K"); // if OS isn't Mac change Visual indication of search field
-    }
-    
-    // Show & hide seach input when focusin & focusout
-    $('#search-input').focus(() => {
-        $('#search-shortcut-sidebar').hide();
-    });
-    $('#search-input').focusout(() => {
-        $('#search-shortcut-sidebar').show();
-    });
-
-    // For Search page
-            
+ $(document).ready(() => {
     if (window.location.pathname === '/search.html') {
         $('form.bd-search').css('visibility','hidden'); // Hide Sidebar Search field
+
+        $(document).keydown(function(event) {
+            if ((event.ctrlKey || event.metaKey) && event.key == "k") {
+                event.preventDefault();
+                $('#q').focus();
+            }
+        });
+    } else {
+        $(document).keydown(function(event) {
+            if ((event.ctrlKey || event.metaKey) && event.key == "k") {
+                event.preventDefault();
+                $('#search-input').focus();
+            }
+        });
     }
+
     if (navigator.platform.indexOf('Mac') === -1) {
-        $('#search-shortcut-page').html("^K"); // if OS isn't Mac change Visual indication of search field
+        $('#search-shortcut').html("^"); // if OS isn't Mac change visual indication of search field
     }
-    $(document).keydown(function(event) {
-        if ((event.ctrlKey || event.metaKey) && event.key == "k") {
-          event.preventDefault();
-          $('#q').focus();
-        }
-    });
+
 });
--- docs/conf.py
+++ docs/conf.py
@@ -129,10 +129,7 @@ exclude_patterns = [
     "volto/developer-guidelines/branch-policy.md",
 ]
 
-html_js_files = [
-    "patch_scrollToActive.js",
-    "search_shortcut.js"
-]
+html_js_files = ["patch_scrollToActive.js", "search_shortcut.js"]
 
 html_extra_path = [
     "robots.txt",
@@ -250,7 +247,7 @@ html_theme_options = {
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
-    "search_bar_text": "Search the docs…",
+    "search_bar_text": "Search",
     "switcher": {
         "json_url": "/_static/switcher.json",
         "version_match": version,
