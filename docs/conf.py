a0cb2d7758043ccc5a5f783f28fb584f82a4d389
Steve Piercy <web@stevepiercy.com>
2023-02-27 12:58:03 -0800

    Bump Plone version to 6.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -322,7 +322,7 @@ def source_replace(app, docname, source):
 # Dict of replacements.
 source_replacements = {
     "{PLONE_BACKEND_MINOR_VERSION}": "6.0",
-    "{PLONE_BACKEND_PATCH_VERSION}": "6.0.1",
+    "{PLONE_BACKEND_PATCH_VERSION}": "6.0.2",
     "{NVM_VERSION}": "0.39.3",
 }
 
