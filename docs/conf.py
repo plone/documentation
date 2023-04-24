85ae17eea89e09ce8b0fca95c0a6ce4ee82a3c3f
Maurits van Rees <maurits@vanrees.org>
2023-04-24 23:51:18 +0200

    Update backend patch version to 6.0.4.

--- docs/conf.py
+++ docs/conf.py
@@ -323,7 +323,7 @@ def source_replace(app, docname, source):
 # Dict of replacements.
 source_replacements = {
     "{PLONE_BACKEND_MINOR_VERSION}": "6.0",
-    "{PLONE_BACKEND_PATCH_VERSION}": "6.0.3",
+    "{PLONE_BACKEND_PATCH_VERSION}": "6.0.4",
     "{NVM_VERSION}": "0.39.3",
 }
 
