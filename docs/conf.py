4344e0e74a779ed412890967855f99a82e763087
Steve Piercy <web@stevepiercy.com>
2024-05-16 18:21:16 -0700

    Attempt to make plone.api importable to Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@ from datetime import datetime
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath("."))
+import os
+import sys
+# Attempt to make plone.api importable to Sphinx
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 
 # -- Project information -----------------------------------------------------
