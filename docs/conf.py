dbe975a02a95d564dfceb6167658f75bef59ee53
Steve Piercy <web@stevepiercy.com>
2024-05-16 20:43:10 -0700

    Attempt to make plone.api importable to Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@ from datetime import datetime
 import os
 import sys
 # Attempt to make plone.api importable to Sphinx
-sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("."))
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
-
+sys.path.insert(0, os.path.abspath("../submodules/plone.api/src"))
 
 # -- Project information -----------------------------------------------------
 
